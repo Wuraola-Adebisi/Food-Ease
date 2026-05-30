fd44a7734bad8aa575232bf637ee99a40b6f3627
Wuraola Adebisi <the.wuraola.adebisi@gmail.com>
2026-05-30 16:44:34 +0100

    Create useDebouncedValue.js

---
+++ src/hooks/useDebouncedValue.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+export function useDebouncedValue(value, delay) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => clearTimeout(timer);
+  }, [delay, value]);
+
+  return debouncedValue;
+}
